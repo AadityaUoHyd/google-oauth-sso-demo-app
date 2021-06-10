718eddbb0e057d129457a09b8cdcaa23bbd4958f
Aaditya Bachchu Chatterjee <aadiraj48@gmail.com>
2021-06-10 21:55:33 +0530

    springBoot-google-oauth-sso-demo-code commit

---
+++ google-oauth-sso-demo-app/src/main/java/org/aadi/google/oauth/GoogleOauthSsoDemoAppApplication.java
@@ -0,0 +1,29 @@
+package org.aadi.google.oauth;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GoogleOauthSsoDemoAppApplication {
+	
+	@GetMapping
+	public String welcome() {
+		return "Welcome to Google!";
+	}
+	
+	@GetMapping("/user")
+	public Principal user(Principal principal) {
+		System.out.println("UserName : " + principal.getName());
+		return principal;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleOauthSsoDemoAppApplication.class, args);
+	}
+
+}
---
+++ google-oauth-sso-demo-app/src/test/java/org/aadi/google/oauth/GoogleOauthSsoDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aadi.google.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleOauthSsoDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
